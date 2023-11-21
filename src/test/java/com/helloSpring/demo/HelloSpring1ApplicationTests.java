54c1c727642007499b2d4e397ca32ea4b41f7070
Roopa <roopa.konaparthi@gmail.com>
2023-11-21 17:50:02 +0530

    initial commit

---
+++ src/test/java/com/helloSpring/demo/HelloSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.helloSpring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
